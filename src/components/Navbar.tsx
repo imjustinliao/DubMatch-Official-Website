c1619d0475daff484ba7996aa64375accb804e4e
Justin Liao <justcs123only@gmail.com>
2025-11-09 18:36:39 -0800

    v0.5 - Final 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,8 @@ type NavbarProps = {
 
 const navLinks = [
   { path: '/', label: 'Home' },
-  { path: '/contact', label: 'Contact' },
   { path: '/about', label: 'About' },
+  { path: '/contact', label: 'Contact' },
 ]
 
 const navLinkClasses =
